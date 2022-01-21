2084566b0f2e06bb6f723bc5629bb4fe70648fc4
khushabu <khushabusingh3833@gmail.com>
2022-01-21 14:21:04 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+function openBtn(){
+  document.getElementById('navbar').style.width='300px';
+  document.getElementById('nav-btn').style.marginLeft='250px';
+}
+function closeBtn(){
+  document.getElementById('navbar').style.width='0';
+  document.getElementById('nav-btn').style.marginLeft='0px';
+}
